ac1dbcd67e0b3d0351554b8bec7f6504e9fe6142
claudioabbate2123-cmyk <claudioabbate21.23@gmail.com>
2026-01-14 22:25:35 +0000

    Fix build

---
+++ app/didattica/materialedidattico/page.tsx
@@ -0,0 +1,8 @@
+export default function MaterialedidatticoPage() {
+  return (
+    <div>
+      <h1>Materiale didattico</h1>
+      <p>Pagina materiale didattico in costruzione.</p>
+    </div>
+  );
+}
